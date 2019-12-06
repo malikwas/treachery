0d9a1baaae0b6edfc3efdb7877fe591b7822f92f
Kindeep <kk17xg@brocku.ca>
2019-12-05 20:07:40 -0500

    Firestore setup

--- app/src/main/java/me/kindeep/treachery/MainActivity.kt
+++ app/src/main/java/me/kindeep/treachery/MainActivity.kt
@@ -2,11 +2,23 @@ package me.kindeep.treachery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.FirebaseApp
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
+
+    val firestore = FirebaseFirestore.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        /*
+        Document snapshot to POJO
+
+        val snapshot: DocumentSnapshot = ...
+        val myObject = snapshot.get<MyClass>("fieldPath")
+         */
+
     }
 }
