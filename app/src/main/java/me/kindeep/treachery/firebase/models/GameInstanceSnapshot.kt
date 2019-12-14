c9253e0e5c4b46661d271b6df8d820c0bc79f26f
wmlk <malik.w.as@gmail.com>
2019-12-14 15:36:43 -0500

    Added new guesses array to GameInstanceSnapshot

--- app/src/main/java/me/kindeep/treachery/firebase/models/GameInstanceSnapshot.kt
+++ app/src/main/java/me/kindeep/treachery/firebase/models/GameInstanceSnapshot.kt
@@ -12,6 +12,7 @@ data class GameInstanceSnapshot(
     val gameId: String = "Loading...",
     val snapshotVersion: String = "0.1",
     val messages: List<MessageSnapshot> = mutableListOf(),
+    val guesses: List<GuessSnapshot> = listOf(),
     val players: MutableList<PlayerSnapshot> = mutableListOf(
         PlayerSnapshot("Mr. Player"),
         PlayerSnapshot("Ms. Bot"),
