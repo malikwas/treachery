a68c1f554242a0ea0596830a88737740a7792f73
Kindeep <kk17xg@brocku.ca>
2019-12-14 22:49:26 -0500

    Forensic cards in player activity and styles

--- app/src/main/java/me/kindeep/treachery/player/PlayerActivity.kt
+++ app/src/main/java/me/kindeep/treachery/player/PlayerActivity.kt
@@ -8,36 +8,72 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.viewpager.widget.ViewPager
 import androidx.viewpager2.widget.ViewPager2
+import kotlinx.android.synthetic.main.joined_players_list_item.*
 import me.kindeep.treachery.R
+import me.kindeep.treachery.firebase.addOnGameUpdateListener
 import me.kindeep.treachery.firebase.models.CardSnapshot
+import me.kindeep.treachery.firebase.models.ForensicCardSnapshot
 import me.kindeep.treachery.firebase.models.GameInstanceSnapshot
 import me.kindeep.treachery.forensic.ForensicViewModel
 import me.kindeep.treachery.shared.CardFragment
+import me.kindeep.treachery.shared.SingleForensicCardFragment
 
 /**
  * Displays every other player, their cards and a messages box for discussion.
  */
 class PlayerActivity : AppCompatActivity() {
     lateinit var gameId: String
+    lateinit var playerName: String
 
     lateinit var viewModel: PlayerViewModel
 
     //    lateinit var testFrag: SinglePlayerFragment
     lateinit var playerPager: ViewPager2
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        gameId = intent?.extras?.getString("gameId")!!
-//        findViewById<TextView>(R.id.gameId).text = gameId
+    lateinit var frag1: SingleForensicCardFragment
+    lateinit var frag2: SingleForensicCardFragment
+    lateinit var frag3: SingleForensicCardFragment
+    lateinit var frag4: SingleForensicCardFragment
+    lateinit var frag5: SingleForensicCardFragment
+    lateinit var frag6: SingleForensicCardFragment
 
-        val playerName = intent?.extras?.getString("playerName")!!
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_player)
+        playerName = intent?.extras?.getString("playerName")!!
+        gameId = intent?.extras?.getString("gameId")!!
+        findViewById<TextView>(R.id.gameId).text = gameId
+        findViewById<TextView>(R.id.player_name).text = playerName
+
+        frag1 = supportFragmentManager.findFragmentById(R.id.card1) as SingleForensicCardFragment
+        frag2 = supportFragmentManager.findFragmentById(R.id.card2) as SingleForensicCardFragment
+        frag3 = supportFragmentManager.findFragmentById(R.id.card3) as SingleForensicCardFragment
+        frag4 = supportFragmentManager.findFragmentById(R.id.card4) as SingleForensicCardFragment
+        frag5 = supportFragmentManager.findFragmentById(R.id.card5) as SingleForensicCardFragment
+        frag6 = supportFragmentManager.findFragmentById(R.id.card6) as SingleForensicCardFragment
+
+        addOnGameUpdateListener(gameId) {
+            frag1.forensicCardSnapshot = it.otherCards.getOrElse(0) { ForensicCardSnapshot() }
+            frag1.bind()
+            frag4.forensicCardSnapshot = it.otherCards.getOrElse(1) { ForensicCardSnapshot() }
+            frag4.bind()
+            frag5.forensicCardSnapshot = it.otherCards.getOrElse(2) { ForensicCardSnapshot() }
+            frag5.bind()
+            frag6.forensicCardSnapshot = it.otherCards.getOrElse(3) { ForensicCardSnapshot() }
+            frag6.bind()
+
+            frag2.forensicCardSnapshot = it.causeCard
+            frag2.bind()
+            frag3.forensicCardSnapshot = it.locationCard
+            frag3.bind()
+        }
 
         PlayerViewModel.gameId = gameId
         PlayerViewModel.playerName = playerName
 
         viewModel = ViewModelProviders.of(this)
             .get(PlayerViewModel::class.java)
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_player)
+//        setContentView(R.layout.activity_player)
 
         playerPager = findViewById(R.id.player_pager)
         doAdapterStuff()
@@ -45,12 +81,7 @@ class PlayerActivity : AppCompatActivity() {
         viewModel.gameInstance.observe(this, Observer {
             log(it)
             findViewById<TextView>(R.id.gameId).text = it.gameId
-//            updateViews()
-//            testFrag.player = viewModel.getPlayer()
-//            doAdapterStuff()
             playerPager.offscreenPageLimit = viewModel.gameInstance.value!!.players.size
-//            doAdapterStuff()
-//            playerPager.adapter!!.notifyDataSetChanged()
         })
 
 
