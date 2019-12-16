e26822cde620d156b2eb3d71b3ef7941f3e9daf5
Kindeep <kk17xg@brocku.ca>
2019-12-16 02:51:27 -0500

    timer fix forensic

--- app/src/main/java/me/kindeep/treachery/forensic/ForensicViewModel.kt
+++ app/src/main/java/me/kindeep/treachery/forensic/ForensicViewModel.kt
@@ -62,21 +62,24 @@ class ForensicViewModel : ViewModel() {
     var allowedCards = 0
 
     fun startTimer() {
-        sendForensicMessage(
-            gameId,
-            "Location and Cause of Death cards set, a new card will be added every ${SINGLE_CARD_TIME / 1000} seconds."
-        )
-        // Starts after the first two cards have been placed.
+        if (!timerStarted) {
+            timerStarted = true
+            sendForensicMessage(
+                gameId,
+                "Location and Cause of Death cards set, a new card will be added every ${SINGLE_CARD_TIME / 1000} seconds."
+            )
+            // Starts after the first two cards have been placed.
 
-        // Add one to allowed cards after fixed time
+            // Add one to allowed cards after fixed time
 
-        val numTimers = gameInstance.value!!.otherCards.size
+            val numTimers = gameInstance.value!!.otherCards.size
 
-        for (i in 0 until numTimers) {
-            val job = GlobalScope.launch(Dispatchers.Main) {
-                delay(SINGLE_CARD_TIME * i)
-                allowedCards++
-                setNextCardValue()
+            for (i in 0 until numTimers) {
+                val job = GlobalScope.launch(Dispatchers.Main) {
+                    delay(SINGLE_CARD_TIME * i)
+                    allowedCards++
+                    setNextCardValue()
+                }
             }
         }
     }
@@ -93,10 +96,7 @@ class ForensicViewModel : ViewModel() {
                 nextCardSnapshots.value = cardsResourcesSnapshot.forensicCards.locationCards
             }
             ForensicGameState.OTHER_CARD -> {
-                if (!timerStarted) {
-                    timerStarted = true
-                    startTimer()
-                }
+                startTimer()
                 for (card in gameInstance.value!!.otherCards.subList(0, allowedCards)) {
                     if (!card.isSelected()) {
                         nextCardSnapshots.value = listOf(card)
