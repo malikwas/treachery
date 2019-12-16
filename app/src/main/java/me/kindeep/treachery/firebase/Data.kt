c1510341b9ac11428683e3e812923f0066f882e4
wmlk <malik.w.as@gmail.com>
2019-12-16 16:01:59 -0500

    whereGreaterThan

--- app/src/main/java/me/kindeep/treachery/firebase/Data.kt
+++ app/src/main/java/me/kindeep/treachery/firebase/Data.kt
@@ -29,8 +29,8 @@ fun activeGamesQuery(): Query {
     val firestore = FirebaseFirestore.getInstance()
     return firestore.collection("games")
         .whereEqualTo("started", false)
-        .orderBy("expiredTimestamp", Query.Direction.DESCENDING)
-        .whereLessThan("expiredTimestamp", Timestamp.now())
+        .orderBy("expiredTimestamp", Query.Direction.ASCENDING)
+        .whereGreaterThan("expiredTimestamp", Timestamp.now())
         .orderBy("createdTimestamp", Query.Direction.DESCENDING)
 }
 
